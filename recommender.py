12635518f93dcbbaebbb484e55818983af07072e
pasham <pasham@umich.edu>
2023-08-13 11:06:44 -0400

    added some functionality to clean up the code. Implemented the genre counter. TODO: finish publisher counter

--- recommender.py
+++ recommender.py
@@ -1,38 +1,51 @@
+#!/usr/bin/python
+
 import pandas as pd
 from IPython.display import display
 
 vg_data = pd.read_csv("games-data.csv")
 #formats data to only lowercase
 vg_data['name'] = vg_data['name'].str.lower()
+vg_data['platform'] = vg_data['platform'].str.lower()
 
-game_1 = input('What is your favorite video game released on or before November 2020?\n').lower()
-while not game_1 in vg_data['name'].unique():
-    game_1 = input('Could not find game. Try entering again:\n').lower()
+def find_game(game_name):
+    
+    while not game_name in vg_data['name'].unique():
+        game_name = input('Could not find game. Try entering again:\n').lower()
 
-game_2 = input('What is your second favorite video game released on or before November 2020?\n').lower()
-while not game_2 in vg_data['name'].unique():
-    game_2 = input('Could not find game. Try entering again:\n').lower()
+    #turns that data into a game entry
+    game = vg_data.loc[vg_data['name'] == game_name]
 
-game_3 = input('What is your third favorite video game released on or before November 2020?\n').lower()
-while not game_3 in vg_data['name'].unique():
-    game_3 = input('Could not find game. Try entering again:\n').lower()
+    if game.shape[0] > 1:
+        #we only want one game, so we find which game the user meant
+        platform = input('Found duplicates of game specified. What platform was your game released on?\n')
+        game = game.loc[game['platform'] == platform]
+    
+    return game
 
-user_games = [game_1, game_2, game_3]
+
+#finds the user's favorite game
+game1 = find_game(input('What is your favorite video game released on or before November 2020?\n').lower())
+
+#does the same for the second favorite game and the third
+game2 = find_game(input('What is your second favorite video game released on or before November 2020?\n').lower())
+game3 = find_game(input('What is your third favorite video game released on or before November 2020?\n').lower())
+
+user_games = [game1, game2, game3]
 
 #dictionaries counting the frequency of genres and publishers from user data
 genre_frequency = {}
 publisher_frequency = {}
 
-all_genres = vg_data.loc[vg_data['name'] == game_1]
-display(all_genres)
-#print(vg_data.loc[vg_data['name'] == game_1])
-#for game in user_games:
-    #creates string containing all the genres of the game
- #   all_genres = vg_data.loc[vg_data['name'] == game]['genre']
-  #  genres = all_genres.split(',')
+for game in user_games:
+    # [0,6] is the location of the genre of the game
+    all_genres = game.iloc[0,6]
+    #list of every single genre
+    genres = all_genres.split(',')
 
-   # for genre in genres:
-    #    if genre in genre_frequency:
-     #       genre_frequency[genre] += 1
-      #  else:
-       #     genre_frequency[genre] = 0
+    for genre in genres:
+        if genre in genre_frequency:
+            genre_frequency[genre] += 1
+        else:
+            genre_frequency[genre] = 1
+print(genre_frequency)
