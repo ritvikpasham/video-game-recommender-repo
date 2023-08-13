4d50499f0083bdeab28084522a1d4ac5709b63df
pasham <pasham@umich.edu>
2023-08-13 13:41:32 -0400

    Added count publisher functionality

--- recommender.py
+++ recommender.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python
-
 import pandas as pd
 from IPython.display import display
 
 vg_data = pd.read_csv("games-data.csv")
-#formats data to only lowercase
+# formats data to only lowercase
 vg_data['name'] = vg_data['name'].str.lower()
 vg_data['platform'] = vg_data['platform'].str.lower()
 
@@ -13,13 +11,13 @@ def find_game(game_name):
     while not game_name in vg_data['name'].unique():
         game_name = input('Could not find game. Try entering again:\n').lower()
 
-    #turns that data into a game entry
+    # turns that data into a game entry
     game = vg_data.loc[vg_data['name'] == game_name]
 
     if game.shape[0] > 1:
-        #we only want one game, so we find which game the user meant
+        # we only want one game, so we find which game the user meant
         platform = input('Found duplicates of game specified. What platform was your game released on?\n').lower()
-        #make sure user inputed a valid platform name
+        # make sure user inputed a valid platform name
         while not platform in vg_data['platform'].unique():
             platform = input('Could not find platform. Try entering again:\n').lower()
 
@@ -28,28 +26,36 @@ def find_game(game_name):
     return game
 
 
-#finds the user's favorite game
+# finds the user's favorite game
 game1 = find_game(input('What is your favorite video game released on or before November 2020?\n').lower())
 
-#does the same for the second favorite game and the third
+# does the same for the second favorite game and the third
 game2 = find_game(input('What is your second favorite video game released on or before November 2020?\n').lower())
 game3 = find_game(input('What is your third favorite video game released on or before November 2020?\n').lower())
 
 user_games = [game1, game2, game3]
 
-#dictionaries counting the frequency of genres and publishers from user data
+# dictionaries counting the frequency of genres and publishers from user data
 genre_frequency = {}
 publisher_frequency = {}
 
 for game in user_games:
     # [0,6] is the location of the genre of the game
-    all_genres = game.iloc[0,6]
-    #list of every single genre
-    genres = all_genres.split(',')
+    # genres is a list of all the genres this game has
+    genres = game.iloc[0,6].split(',')
 
     for genre in genres:
         if genre in genre_frequency:
             genre_frequency[genre] += 1
         else:
             genre_frequency[genre] = 1
+    
+    # now counts publishers
+    # [0,1] is the location of the publisher of the game
+    publisher = game.iloc[0,1]
+    if publisher in publisher_frequency:
+        publisher_frequency[publisher] += 1
+    else:
+        publisher_frequency[publisher] = 1
 print(genre_frequency)
+print(publisher_frequency)
